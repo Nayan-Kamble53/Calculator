3cab52c7f929065eb581a7be4bddfe7cd0432691
Nayan-Kamble53 <96567359+Nayan-Kamble53@users.noreply.github.com>
2022-08-22 20:50:06 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+let outputScreen = document.getElementById("output-screen");
+
+function display(num){
+    outputScreen.value += num;
+}
+
+function Clear(){
+    outputScreen.value = "";
+}
+
+function Calculate(){
+    try{
+        outputScreen.value = eval(outputScreen.value);
+    }
+
+    catch(err){
+        alert("Invalid");
+    }
+}
+
+function del(){
+    outputScreen.value = outputScreen.value.slice(0,-1);
+}
